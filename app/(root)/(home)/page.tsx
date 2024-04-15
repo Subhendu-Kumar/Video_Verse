f6e7578d573d1d79ab5c7e509a9c274c7a716127
Subhendu-Kumar <subhendukumardutta330@gmail.com>
2024-04-15 17:22:43 +0530

    changed something

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,8 +1,38 @@
-import React from "react";
+import MeetingTypeList from "@/components/MeetingTypeList";
 
 const Home = () => {
+  const currentDate = new Date();
+
+  const formattedDate = currentDate.toLocaleDateString("en-IN", {
+    weekday: "long",
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  });
+
+  const FormattedTime = currentDate.toLocaleTimeString("en-IN", {
+    hour12: true,
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+
   return (
-  <div> Home</div>
+    <section className="flex size-full flex-col gap-10 text-white">
+      <div className="h-[270px] w-full rounded-[20px] bg-cover bg-hero">
+        <div className="flex flex-col h-full justify-between max-md:px-5 max-md:py-8 lg:p-8">
+          <h2 className="glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal">
+            Upcoming Meeting at: 11:30 pm
+          </h2>
+          <div className="flex flex-col gap-2">
+            <h1 className="text-4xl font-extrabold uppercase">
+              {FormattedTime}
+            </h1>
+            <p className="font-light text-sky-1">{formattedDate}</p>
+          </div>
+        </div>
+      </div>
+      <MeetingTypeList />
+    </section>
   );
 };
 
---
+++ components/MobileNav.tsx
@@ -0,0 +1,75 @@
+"use client";
+import React from "react";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import Image from "next/image";
+import Link from "next/link";
+import { SidebarLinks } from "@/constants";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+
+const MobileNav = () => {
+  const pathname = usePathname();
+  return (
+    <section className="w-full max-w-[264px]">
+      <Sheet>
+        <SheetTrigger asChild>
+          <Image
+            src="/icons/hamburger.svg"
+            alt="Hamburger menu buton"
+            width={34}
+            height={34}
+            className="cursor-pointer sm:hidden"
+          />
+        </SheetTrigger>
+        <SheetContent className="bg-dark-1 border-none" side="left">
+          <Link href="/" className="flex items-center gap-1">
+            <Image
+              src="/icons/logo.svg"
+              alt="video verse logo"
+              width={32}
+              height={32}
+              className="max-sm:size-10"
+            />
+            <p className="text-[26px] font-extrabold text-white">Video Verse</p>
+          </Link>
+          <div className="flex h-[calc(100vh-72px)] flex-col justify-between overflow-y-auto">
+            <SheetClose asChild>
+              <section className="flex h-full flex-col gap-6 pt-16 text-white">
+                {SidebarLinks.map((link) => {
+                  const isActive = pathname === link.route;
+                  return (
+                    <SheetClose asChild key={link.route}>
+                      <Link
+                        href={link.route}
+                        key={link.label}
+                        className={cn(
+                          "flex items-center w-full max-w-60 gap-4 p-4 rounded-lg",
+                          { "bg-blue-1": isActive }
+                        )}
+                      >
+                        <Image
+                          src={link.imgUrl}
+                          alt={link.label}
+                          width={20}
+                          height={20}
+                        />
+                        <p className="font-semibold">{link.label}</p>
+                      </Link>
+                    </SheetClose>
+                  );
+                })}
+              </section>
+            </SheetClose>
+          </div>
+        </SheetContent>
+      </Sheet>
+    </section>
+  );
+};
+
+export default MobileNav;
